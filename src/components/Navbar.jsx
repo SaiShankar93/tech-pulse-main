49089b82be76173b8faefc8beacf564fe55b2b64
Mithun Kumar <109142302+mithun-1603@users.noreply.github.com>
2024-02-28 23:44:16 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ export default function Example() {
             <a href="#" className="-m-1.5 p-1.5">
               <img
                 className="h-8 w-auto"
-                src="/src/images/Codehub.png?color=indigo&shade=600"
+                src="/src/images/Codehub.png"
                 alt=""
               />
             </a>
